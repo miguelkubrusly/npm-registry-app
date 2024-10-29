a6526dcfc157ef812fad958ff30e4dc64f7d44e1
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-10-29 19:20:35 -0300

    feat(root): create Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+import SearchInput from "./SearchingInput";
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <Link to={"/"}>NPM Registry</Link>
+      <SearchInput />
+    </div>
+  );
 }
 
 export default Header;
