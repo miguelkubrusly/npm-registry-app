2366ce3395773b04cb815e017d995f8431d0a3c4
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-10-29 19:39:42 -0300

    style(root): 🎨

--- src/components/SearchingInput.tsx
+++ src/components/SearchingInput.tsx
@@ -1,22 +1,10 @@
 // ====================================================== //
-// ======== simplest way of creating Form routing: ======= //
-// ====================================================== //
-/* 
-import { Form } from "react-router-dom";
-
-function SearchInput() {
-  return <Form action="/search">
-    <input name="term"/>
-  </Form>
-  
-} */
-
-// ====================================================== //
 // ==== more customizable way of programmatic routing: === //
 // ====================================================== //
 
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { VscSearch } from "react-icons/vsc";
 
 function SearchInput() {
   const nav = useNavigate();
@@ -28,18 +16,36 @@ function SearchInput() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="border border-b-2">
-      <label htmlFor="term">Search</label>
-      <input
-        name="term"
-        type="text"
-        value={term}
-        onChange={(e) => {
-          setTerm(e.target.value);
-        }}
-      />
+    <form onSubmit={handleSubmit}>
+      <div className="relative">
+        <div className="absolute inset-y-0 flex items-center pl-3">
+          <VscSearch className="h-5 w-5 text-gray-500" />
+        </div>
+        <input
+          type="text"
+          value={term}
+          onChange={(e) => {
+            setTerm(e.target.value);
+          }}
+          className="pl-10 py-2 w-full border-0 shadow-none"
+          placeholder="Search packages"
+        />
+      </div>
     </form>
   );
 }
 
 export default SearchInput;
+
+// ====================================================== //
+// ======== simplest way of creating Form routing: ======= //
+// ====================================================== //
+/* 
+import { Form } from "react-router-dom";
+
+function SearchInput() {
+  return <Form action="/search">
+    <input name="term"/>
+  </Form>
+  
+} */
