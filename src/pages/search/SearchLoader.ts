a0495d7c6c413a7c45740fb53ebf1afca5112fd3
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-10-29 22:43:24 -0300

    refactor(search): return package summary

--- src/pages/search/SearchLoader.ts
+++ src/pages/search/SearchLoader.ts
@@ -3,14 +3,25 @@ import { fetchPackages } from "../../api/queries/fetchPackages";
 
 async function SearchLoader({
   request,
-}: LoaderFunctionArgs<Request>): Promise<Package[]> {
+}: LoaderFunctionArgs<Request>): Promise<PackageSummary[]> {
   const { searchParams } = new URL(request.url);
   const term = searchParams.get("term");
   if (!term) {
     throw new Error("Search parameters must be provided.");
   }
-  const packages = await fetchPackages(term);
-  return packages;
+  const packages: Package[] = await fetchPackages(term);
+  const summary: PackageSummary[] = packages.map(
+    ({ name, version, description, keywords, links }) => {
+      return {
+        name,
+        version,
+        description,
+        keywords,
+        links,
+      };
+    }
+  );
+  return summary;
 }
 
 export default SearchLoader;
